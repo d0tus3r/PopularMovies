b89fa7f36ff2e6d24148fd06922307e9e3c40914
Andrew Deese <d0tus3r@gmail.com>
2018-05-21 15:22:05 -0400

    completes json parsing, starts putting it all together

--- app/src/main/java/net/digitalswarm/popularmovies/MainActivity.java
+++ app/src/main/java/net/digitalswarm/popularmovies/MainActivity.java
@@ -1,12 +1,20 @@
 package net.digitalswarm.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import net.digitalswarm.popularmovies.models.Movie;
+import net.digitalswarm.popularmovies.utils.MovieJsonUtil;
+import net.digitalswarm.popularmovies.utils.NetUtils;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 //TODO: Implement json movie poster query
@@ -17,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     //init grid layout manager for main layout
     private GridLayoutManager gridLayout;
     private List<Movie> moviePosterList;
+    private ArrayList<Movie> movieArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +49,42 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private class GetMovies extends AsyncTask<URL, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //TODO: add toast message
+        }
+        /**
+         * feed url for netutil request, return json for post execute
+         * @param url
+         * @return
+         */
+        @Override
+        protected String doInBackground(URL... url) {
+            String movieResults = null;
+            try {
+                movieResults = NetUtils.getMovieData(url[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return movieResults;
+        }
+        @Override
+        protected void onPostExecute(String movieResults){
+            //parse json and build array of movies for mp recycler view
+            if (movieResults != null) {
+
+                try {
+                    movieArrayList = MovieJsonUtil.getMovieResults(movieResults);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
     }
 }
--- app/src/main/java/net/digitalswarm/popularmovies/utils/MovieJsonUtil.java
+++ app/src/main/java/net/digitalswarm/popularmovies/utils/MovieJsonUtil.java
@@ -18,24 +18,32 @@ import org.json.JSONObject;
 public class MovieJsonUtil {
 
     //TMDB Keys for parsing JSON
+    //results json is an array of movies
     private static final String TMDB_RESULTS = "results";
     private static final String TMDB_POSTER_URL = "poster_path";
     private static final String TMDB_TITLE = "original_title";
-    private static final String TMDB_ID = "id";
-    private static final String TMDB_POPULARITY = "popularity";
     private static final String TMDB_RATING = "vote_average";
     private static final String TMDB_RELEASE = "release_date";
     private static final String TMDB_OVERVIEW = "overview";
 
-    //array of movie objects
-    private ArrayList<Movie> movieArrayList;
-
-    //json objects
-
     //Net Utils will give a String of json data - use this data to create a json array to convert to movie objects
-
-
-
-
-
+    public static ArrayList getMovieResults(String tmdbJsonResponse) throws JSONException {
+        //assign tmdb response to jsonobject
+        JSONObject tmdbJson = new JSONObject(tmdbJsonResponse);
+        //strip results array | results is an array with each
+        JSONArray movieResultsJson = tmdbJson.getJSONArray(TMDB_RESULTS);
+        //array of movie objects
+        ArrayList<Movie> movieArrayList = new ArrayList<>(movieResultsJson.length());
+        //loop through movieResultsJson and return an array of movie objects
+        for (int i = 0; i < movieResultsJson.length(); i++){
+            JSONObject resultEntry = movieResultsJson.getJSONObject(i);
+            movieArrayList.set(i, new Movie(
+                    resultEntry.getString(TMDB_TITLE),
+                    resultEntry.getString(TMDB_POSTER_URL),
+                    resultEntry.getString(TMDB_RELEASE),
+                    resultEntry.getString(TMDB_RATING),
+                    resultEntry.getString(TMDB_OVERVIEW)));
+        }
+        return movieArrayList;
+    }
 }
